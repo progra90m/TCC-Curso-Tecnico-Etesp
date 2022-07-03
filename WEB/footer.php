ec8cde9c765719016f5d77bc201f6a2fe934001d
Aaron Ferraz <aaronferraz8@gmail.com>
2022-07-02 22:30:52 -0300

    avanço1

---
+++ WEB/footer.php
@@ -0,0 +1,11 @@
+<?php
+    echo "
+    <footer> 
+        <div class='Rodape'> 
+            <p>
+                &copy; 2022 Etesp Universe
+            </p>
+        </div>
+    </footer>
+    ";
+?>
--- WEB/index.php
+++ WEB/index.php
@@ -5,11 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS/estilo.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="CSS/footer.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="CSS/menu.css" media="screen" />
     <title> Home </title>
 </head>
 <body>
     <?php
         require "menu.php";
+    ?>
+    <main>
+        
+    </main>
+    <?php
+        require "footer.php";
     ?>
 </body>
 </html>
